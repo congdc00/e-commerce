8c6caab989b81b81897c310c1b1173099192741e
DINH CHI CONG <dinhchicongf9@gmail.com>
2022-01-14 17:37:52 +0700

    e commerce

--- ui/e_commerce/home/admin.py
+++ ui/e_commerce/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'number']
+    list_filter = ['price']
+    search_fields = ['title']
+admin.site.register(Product, ProductAdmin)
--- ui/e_commerce/home/models.py
+++ ui/e_commerce/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length = 200)
+    price = models.IntegerField()
+    number = models.IntegerField()
+    infomation = models.TextField()
+    location = models.TextField()
