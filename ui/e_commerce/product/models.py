84e0c7b33ff84b07b7283ddd65483f6b99764c8f
DINH CHI CONG <dinhchicongf9@gmail.com>
2022-01-21 17:07:48 +0700

    update version 0.1.4

--- ui/e_commerce/product/models.py
+++ ui/e_commerce/product/models.py
@@ -2,19 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    title = models.CharField (default='', max_length='')
+    title = models.CharField (default='', max_length=200)
     slug = models.CharField (max_length=100, default='')
     description = models.TextField (default='')
     active = models.BooleanField (default=True)
 
-class Product(models.Model):
-    name = models.CharField(max_length = 200)
-    image = models.ImageField(null = True)
-    price = models.IntegerField()
-    number = models.IntegerField()
-    describe = models.TextField(null = True, max_length = 300)
-    infomation = models.TextField()
-    location = models.TextField()
-
-    def __str__(self):
-        return self.name
